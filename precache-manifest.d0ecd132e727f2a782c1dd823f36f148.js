7d2e4cea3c8b353f3c89261d6d1f8ad2dc02858e
Sibaprasad Maiti <sibaprasad.maiti@varatechnology.com>
2020-03-27 08:29:26 +0530

    Updates

---
+++ precache-manifest.d0ecd132e727f2a782c1dd823f36f148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adc4efd580fea7ec18c75ecc9c282e9",
+    "url": "/tic-tac-toe-reloaded/index.html"
+  },
+  {
+    "revision": "71d1d94e75cf86b48b28",
+    "url": "/tic-tac-toe-reloaded/static/css/main.8fee1580.chunk.css"
+  },
+  {
+    "revision": "8ddf65db3a2db43dfa2c",
+    "url": "/tic-tac-toe-reloaded/static/js/2.34296584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-reloaded/static/js/2.34296584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d1d94e75cf86b48b28",
+    "url": "/tic-tac-toe-reloaded/static/js/main.d7f262be.chunk.js"
+  },
+  {
+    "revision": "85c5ec2f2537063cbd2b",
+    "url": "/tic-tac-toe-reloaded/static/js/runtime-main.684918f3.js"
+  }
+]);
